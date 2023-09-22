b64e4b91b20bb3fa992a859823e6b1a4a4bbce6b
unfeeling-001 <hyanerkaev@gmail.com>
2023-09-22 11:15:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Messages from "./components/Messages";
+import { useState } from 'react';
 function App() {
 
-  
+  const [todos, setTodos] = useState([
+    {
+      text: 'Html'
+    },
+    {
+      text: 'Html'
+    },
+    {
+      text: 'Html'
+    }
+  ])
+
+const [text, setText] = useState('')
+const [check, setCheck] = useState(true)
+
+
+
+  function handleChange(e) {
+    setText(e.target.value)
+    
+  }
+
+
+  function handleBtn() {
+    setTodos([
+      ...todos,
+      {
+        text : text
+    },
+  ]);
+  setText('')
+  }
   
       return (
         
-        <>
-        <Messages/>
-        </>
-        
+      <div className='container pt-5'>
+
+          <div>
+            <input type='checkbox' checked={check}/> 
+            <input placeholder='Text' value={text} onChange={(e) => handleChange(e)}/> 
+            <button onClick={handleBtn}>add</button>
+          </div>
+
+          {todos.map((item, index) => {
+           return (
+          <div key={index}>
+            {item.text}
+          </div>
+            );
+          })}
+     </div>
+      
       );
 
 
